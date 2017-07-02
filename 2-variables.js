3e3001f1931f3cbbaf55ef6e9ae23661c03786b0
oscar diaz <you@example.com>
2017-07-02 16:17:53 -0500

    reto 2 y 3 javascript ok :)

---
+++ 2-variables.js
@@ -0,0 +1,10 @@
+// vas a aprender como declarar variables en JavaScript. 
+// La metodología que vamos a utilizar es un poco diferente.
+// Oprime el botón Evaluar Solución para empezar.
+// Tu solución no va a pasar, pero vas a ver un primer mensaje de error que te va a decir lo que tienes que hacer. Una vez soluciones ese error oprime Evaluar Solución nuevamente para conocer el siguiente error.
+// Sigue así hasta que el reto quede completamente solucionado.
+
+// mi solucion
+var numeroSecreto = 23;
+var clave = "una cadena";
+var permitido = false;
---
+++ 3-mi-primera-funcion.js
@@ -0,0 +1,16 @@
+// crear una función suma que sume dos números y retorne el resultado.
+// Ejemplos:
+
+// suma(1, 2) //=> 3
+// suma(0, 0) //=> 0
+// suma(245, 923) //=> 1168
+
+// mi solucion
+function suma(num1,num2) {
+  resultado = num1 + num2;
+  return resultado;
+}
+
+suma(1, 2) //=> 3
+suma(0, 0) //=> 0
+suma(245, 923) //=> 1168
