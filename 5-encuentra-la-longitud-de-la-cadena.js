bb2ab70b72c0198e8ea842294dc9401796f53d27
oscar diaz <you@example.com>
2017-07-02 16:35:09 -0500

    reto 5 javascript ok :)

---
+++ 5-encuentra-la-longitud-de-la-cadena.js
@@ -0,0 +1,12 @@
+// es crear una función longitud que encuentre la longitud de una cadena (string).
+
+// Ejemplos:
+
+// longitud("") //=> 0
+// longitud("Hola") //=> 4
+// longitud("You Rock") //=> 8 (contando el espacio)
+
+// mi solucion
+function longitud(string){
+	return string.length
+}
