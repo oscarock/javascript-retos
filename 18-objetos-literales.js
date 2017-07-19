d2a1050d12352967431d7e5a5c1ed04f0eb3bf3f
Your Name <you@example.com>
2017-07-19 16:25:54 -0500

    temp

---
+++ 18-objetos-literales.js
@@ -0,0 +1,32 @@
+// crear y manipular objetos literales en JavaScript. Lo vamos a hacer con
+// la metodología de evaluar y solucionar el error.
+//
+// Instrucciones
+//
+// Crea la variable pepito que debe ser igual a un objeto literal
+// Esta variable debe estar declarada antes de la variable maria.
+//
+// Oprime Evaluar solución para mostrar el primer mensaje de error.
+//
+// Soluciona el problema que te indica el mensaje.
+//
+// Repite el paso 2 y 3 hasta que finalices el reto.
+
+var maria = {
+  nombre: "Terah",
+  edad: 32,
+  altura: 1.70,
+  peso: 60,
+  colorPelo: "cafe",
+  colorOjos: "cafe",
+}
+
+var pepito = {
+  nombre: "Pepito",
+}
+
+console.log(maria.esposo())
+//
+// El objeto 'maria' debe tener la propiedad 'esposo' con el valor del objeto 'pepito'
+//
+// El objeto 'pepito' debe tener la propiedad 'nombre' con valor 'Pepito'
